bdf796f55bd3a278cab75fd40292cc8c88d94514
Ismat-Samadov <ismetsemedov@gmail.com>
2023-04-04 08:50:47 +0400

    Correction in biginteger

--- BigInt.java
+++ BigInt.java
@@ -1,19 +1,13 @@
-import java.io.*;
-import java.util.*;
-import java.text.*;
 import java.math.*;
-import java.util.regex.*;
-
-public class BigInt {
-
-    public static void main(String[] args) {
-        /* Enter your code here. Read input from STDIN. Print output to STDOUT. Your class should be named Solution. */
-        Scanner sc = new Scanner(System.in);
-        String numOne = sc.nextLine();
-        String numTwo = sc.nextLine();
-        BigInteger bigNumOne = new BigInteger(numOne);
-        BigInteger bigNumTwo = new BigInteger(numTwo);
-        System.out.println(bigNumOne.add(bigNumTwo));
-        System.out.println(bigNumOne.multiply(bigNumTwo));
-    }
+import java.util.Scanner;
+public class Solution {
+   public static void main(String []args){
+       Scanner input = new Scanner(System.in);
+       String s1,s2;
+       s1=input.nextLine();
+       s2=input.nextLine();
+       input.close();
+       System.out.println(new BigInteger(s1).add(new BigInteger(s2)));
+       System.out.println(new BigInteger(s1).multiply(new BigInteger(s2)));
+   }
 }
